1af92a6f53a4157517c0c4203bdfbfa61dce5d4b
Chuacx13 <chuachongxuan@gmail.com>
2024-08-29 13:29:55 +0800

    29082024-ChongXuan-Initialize Spring Boot

---
+++ src/main/java/com/wealth_health/WealthHealthApplication.java
@@ -0,0 +1,13 @@
+package com.wealth_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthHealthApplication.class, args);
+	}
+
+}
