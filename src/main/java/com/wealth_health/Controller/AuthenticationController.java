4f2de352a0113ad44f1ab9099630cd55237028c6
Chuacx13 <chuachongxuan@gmail.com>
2024-09-01 01:28:25 +0800

    31092024-ChongXuan-Completed Authentication Controller + Login Responses

---
+++ src/main/java/com/wealth_health/Controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.wealth_health.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wealth_health.Model.User;
+import com.wealth_health.Responses.LoginResponse;
+import com.wealth_health.Service.AuthenticationService;
+import com.wealth_health.Service.JwtService;
+import com.wealth_health.Dto.LoginUserDto;
+import com.wealth_health.Dto.RegisterUserDto;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signUp(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+}
