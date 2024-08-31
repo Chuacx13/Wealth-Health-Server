648401166e904c10e04867f4d1910d91dafffc65
Chuacx13 <chuachongxuan@gmail.com>
2024-09-01 01:11:18 +0800

    31092024-ChongXuan-Completed Dto Classes

---
+++ src/main/java/com/wealth_health/Dto/LoginUserDto.java
@@ -0,0 +1,11 @@
+package com.wealth_health.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/wealth_health/Dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package com.wealth_health.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
